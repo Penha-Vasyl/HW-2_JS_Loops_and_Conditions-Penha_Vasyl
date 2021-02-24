8e14124290de1872444f4a4799b5f6ef9b9e61d5
Penha Vasyl <vasylpenha@gmail.com>
2021-02-24 21:37:48 +0200

    Ordered code

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-let userNumberN = '';
-let userNumberM = '';
-
+let userNumberN = null;
+let userNumberM = null;
 let sumNumbers = 0;
+
 do {
   userNumberN = + (prompt('Please write іnitial number'));
   userNumberM = + (prompt('Please write final number'));
-}
-while (isNaN(userNumberN) || isNaN(userNumberM))
+
+  if (userNumberN > userNumberM) {
+    alert('The initial number cannot be greater than the final number. Enter the number again.')
+    continue;
+  }
+  if (isNaN(userNumberN) || isNaN(userNumberM)) {
+    alert('Your numbers is not valid, try again')
+  }
+} while (isNaN(userNumberN) || isNaN(userNumberM))
 
 const integerNumberN = parseInt(userNumberN);
 const integerNumberM = parseInt(userNumberM);
+const skipEvenNumbers = confirm('Whether to skip even numbers?');
 
-
-if (integerNumberN > integerNumberM) {
-  alert('The initial number cannot be greater than the final number. Enter the number again.')
-}
-else {
-  const skipEvenNumbers = confirm('Whether to skip even numbers?');
-  for (let i = integerNumberN; i <= integerNumberM; i++) {
-    if (skipEvenNumbers === false || i % 2 !== 0) {
-      sumNumbers += i;
-    }
+for (let i = integerNumberN; i <= integerNumberM; i++) {
+  if (skipEvenNumbers === false || i % 2 !== 0) {
+    sumNumbers += i;
   }
 }
 
